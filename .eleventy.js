862ab15f7534baf2eab90e99d1f75d193272170e
Jess Hendrix <penguini3_dev@penguini3.anonaddy.com>
2025-11-05 21:53:45 -0600

    local edit

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   // This will stop the default behaviour of foo.html being turned into foo/index.html
   eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
   eleventyConfig.addPassthroughCopy("src/styles");
+  eleventyConfig.addPassthroughCopy("src/images");
   return {
     dir: {
       input: "src",
